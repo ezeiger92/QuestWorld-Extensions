41c5bf0c525ec1e8980bd59393d7853b31545057
Erik <ezeiger92@gmail.com>
2017-12-29 22:46:55 -0800

    Bump version numbers Closes ezeiger92/QuestWorld2#49

--- modules/Citizens/src/main/java/com/questworld/extension/citizens/Citizens.java
+++ modules/Citizens/src/main/java/com/questworld/extension/citizens/Citizens.java
@@ -8,15 +8,19 @@ import me.mrCookieSlime.QuestWorld.api.QuestExtension;
 import me.mrCookieSlime.QuestWorld.api.QuestWorld;
 import me.mrCookieSlime.QuestWorld.api.contract.IMission;
 import me.mrCookieSlime.QuestWorld.api.contract.IPlayerStatus;
+import me.mrCookieSlime.QuestWorld.util.Log;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
 
 import org.bukkit.Particle;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class Citizens extends QuestExtension {
+	FileConfiguration config;
 	private static HashSet<MissionType> types = new HashSet<>();
 	
 	public static NPC npcFrom(IMission instance) {
@@ -50,6 +54,21 @@ public class Citizens extends QuestExtension {
 		
 		for(MissionType type : getMissionTypes())
 			types.add(type);
+		
+		onReload();
+	}
+	
+	@Override
+	public void onSave() {
+		Log.info("Saving citizens extension config");
+		getResourceLoader().saveConfigNoexcept(config, "config-citizens.yml", true);
+	}
+	
+	@Override
+	public void onReload() {
+		Log.info("Loading citizens extension config");
+		config = getConfiguration("config-citizens.yml");
+		Log.info(config.getString("npc_particles.type"));
 	}
 	
 	@Override
@@ -60,7 +79,27 @@ public class Citizens extends QuestExtension {
 			
 			@Override
 			public void run() {
+				ConfigurationSection particleConfig = config.getConfigurationSection("npc_particles");
 				HashSet<Integer> shown = new HashSet<>();
+				
+				
+				Particle particleType = Particle.VILLAGER_HAPPY;
+				try {
+					particleType = Particle.valueOf(particleConfig.getString("type"));
+				}
+				catch(Exception e) {
+				}
+				
+				double nearX = particleConfig.getDouble("nearby.x", 20.0);
+				double nearY = particleConfig.getDouble("nearby.y", 8.0);
+				double nearZ = particleConfig.getDouble("nearby.z", 20.0);
+				
+				int count = particleConfig.getInt("count", 20);
+				int extra = particleConfig.getInt("extra", 0);
+				
+				double spreadX = particleConfig.getDouble("spread.x", 0.5);
+				double spreadY = particleConfig.getDouble("spread.y", 0.7);
+				double spreadZ = particleConfig.getDouble("spread.z", 0.5);
 				
 				for(MissionType type : types)
 					for(IMission mission : QuestWorld.getViewer().getMissionsOf(type)) {
@@ -68,7 +107,7 @@ public class Citizens extends QuestExtension {
 						if (npc != null && npc.getEntity() != null) {
 							ArrayList<Player> players = new ArrayList<>();
 							
-							for (Entity n: npc.getEntity().getNearbyEntities(20D, 8D, 20D)) {
+							for (Entity n: npc.getEntity().getNearbyEntities(nearX, nearY, nearZ)) {
 								if (n instanceof Player && ((Player)n).isOnline()) {
 									IPlayerStatus manager = QuestWorld.getPlayerStatus((Player)n);
 									if (manager.isMissionActive(mission)) {
@@ -80,7 +119,8 @@ public class Citizens extends QuestExtension {
 							if(!shown.contains(npc.getEntity().getEntityId())) {
 								shown.add(npc.getEntity().getEntityId());
 								for(Player p : players) {
-									p.spawnParticle(Particle.VILLAGER_HAPPY, npc.getEntity().getLocation().add(0, 1, 0), 20, 0.5, 0.7, 0.5, 0);
+									p.spawnParticle(particleType, npc.getEntity().getLocation().add(0, 1, 0),
+											count, spreadX, spreadY, spreadZ, extra);
 								}
 							}
 						}
